a69e36e9ace59d097ec6c60582aabd26c8231379
Ritwiksahoo0204 <ritwiksahoo2004@gmail.com>
2024-09-23 03:47:22 +0530

    modification

--- recursion/subsequences.java
+++ recursion/subsequences.java
@@ -1,17 +1,38 @@
 package recursion;
 
+import java.util.HashSet;
+
 public class subsequences {
- public static void subsequences(String str, int index,String newString){
+//  public static void subsequences(String str, int index,String newString){
+//     if (index==str.length()) {
+//         System.out.println(newString);
+//         return;
+//     }
+//     char currChar=str.charAt(index);
+//     subsequences(str, index+1, newString+currChar);
+//     subsequences(str, index+1, newString);
+//  }  
+
+// for unique subsequences
+
+public static void subsequences(String str, int index,String newString,HashSet<String>set){
     if (index==str.length()) {
-        System.out.println(newString);
+       if (set.contains(newString)) {
         return;
+       } else {
+        System.out.println(newString);
+        set.add(newString);
+        return;
+
+       }
     }
     char currChar=str.charAt(index);
-    subsequences(str, index+1, newString+currChar);
-    subsequences(str, index+1, newString);
- }   
+    subsequences(str, index+1, newString+currChar,set);
+    subsequences(str, index+1, newString,set);
+ }
  public static void main(String[] args) {
-    String str="abc";
-    subsequences(str, 0,"");
+    String str="aaa";
+    HashSet<String> name= new HashSet<>();
+    subsequences(str, 0,"",name);
  }
 }
